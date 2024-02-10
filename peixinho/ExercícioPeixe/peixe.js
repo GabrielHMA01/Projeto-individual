e8e84c35160b67950a60dcc70695ddb02c0a8d24
GabrielHMA01 <159061439+GabrielHMA01@users.noreply.github.com>
2024-02-10 12:28:42 -0300

    criar bolhas com movimento, responder perguntas

--- peixinho/ExercícioPeixe/peixe.js
+++ peixinho/ExercícioPeixe/peixe.js
@@ -16,6 +16,8 @@ function preload() {
     //carrega a imagem de fundo
     this.load.image('Mar', 'assets/fundoMar.png');
 
+    this.load.image('bolhas', 'assets/bolhas.png')
+
     //carrega a imagem das algas
     this.load.image ('algas', 'assets/Algas.png');
 
@@ -25,25 +27,39 @@ function preload() {
     //carrega a imagem do peixe
     this.load.image ('peixe', 'assets/peixe/tubarao.png');
     }
+
 function create () {
     //adiciona o fundo
     this.add.image(400, 300, 'Mar');
 
-    //adiciona a imagem dad algas
+    //adiciona a imagem das algas
     this.add.image(120, 500, 'algas') .setScale (0.12);
 
     //adiciona a logo do inteli
     this.add.image(400, 75, 'logo') .setScale (0.5);
 
-    //guarda o peixe em uma variável
+    //guarda o tubarão em uma variável
     tubarao = this.add.image(400, 300, 'peixe');
 
+    //guarda as bolhas em uma variável
+    bolhas = this.add.image(375, 250, 'bolhas') .setScale (0.2)
+
+    //flipa a imagem do tubarão
     tubarao.setFlip(true, false)
+
+    //flipa a iamgem das bolhas
+    bolhas.setFlip(true, false)
     }
+
 function update () {
-    tubarao.x = this.input.x
+    //adiciona movimento ao tubarão
+    tubarao.x = this.input.x;
     tubarao.y = this.input.y
+
+    //adiciona movimentos às bolhas que acompanham o tubarão com um deslocamento no eixo X
+    bolhas.x = this.input.x-100;
+    bolhas.y = this.input.y
 }
     //exibe mensagens antes de iniciar o jogo
-    window.alert ("Bem vindo")
+    window.alert ("Bem vindo");
     window.confirm ("Vamos começar?")
