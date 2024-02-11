ca26f3159899851285c74fcab673a3f7b19bcdc2
GabrielHMA01 <159061439+GabrielHMA01@users.noreply.github.com>
2024-02-11 08:32:35 -0300

    adicionar música, bolhas e corrigir pequenos erros

--- peixinho/ExercícioPeixe/peixe.js
+++ peixinho/ExercícioPeixe/peixe.js
@@ -17,19 +17,29 @@ function preload() {
     this.load.image('Mar', 'assets/fundoMar.png');
 
     //carrega a imagem das bolhas
-    this.load.image('bolhas', 'assets/bolhas.png')
+    this.load.image('bolhas', 'assets/bolhas.png');
 
     //carrega a imagem das algas
-    this.load.image ('algas', 'assets/Algas.png');
+    this.load.image('algas', 'assets/Algas.png');
 
     //carrega a logo do inteli
-    this.load.image ('logo', 'assets/logo-inteli_branco.png');
+    this.load.image('logo', 'assets/logo-inteli_branco.png');
 
     //carrega a imagem do peixe
-    this.load.image ('peixe', 'assets/peixe/tubarao.png');
+    this.load.image('peixe', 'assets/peixe/tubarao.png');
+
+    //carrega a música de fundo
+    this.load.audio('main_theme', 'assets/audio/musica_de_fundo.mp3');
     }
 
 function create () {
+
+    //adiciona música
+    var main_theme = this.sound.add("main_theme", {volume: 0.7} );
+
+    //dá play na música
+    main_theme.play();
+
     //adiciona o fundo
     this.add.image(400, 300, 'Mar');
 
@@ -43,24 +53,24 @@ function create () {
     tubarao = this.add.image(400, 300, 'peixe');
 
     //guarda as bolhas em uma variável
-    bolhas = this.add.image(375, 250, 'bolhas') .setScale (0.2)
+    bolhas = this.add.image(375, 250, 'bolhas') .setScale (0.2);
 
     //flipa a imagem do tubarão
-    tubarao.setFlip(true, false)
+    tubarao.setFlip(true, false);
 
     //flipa a iamgem das bolhas
-    bolhas.setFlip(true, false)
+    bolhas.setFlip(true, false);
     }
 
 function update () {
     //adiciona movimento ao tubarão
     tubarao.x = this.input.x;
-    tubarao.y = this.input.y
+    tubarao.y = this.input.y;
 
     //adiciona movimentos às bolhas que acompanham o tubarão com um deslocamento no eixo X
     bolhas.x = this.input.x-100;
-    bolhas.y = this.input.y
+    bolhas.y = this.input.y;
 }
     //exibe mensagens antes de iniciar o jogo
-    window.alert ("Bem vindo");
-    window.confirm ("Vamos começar?")
+    window.alert ("Bem vindo!");
+    window.confirm ("Vamos começar? obs: O carregamento pode levar alguns instantes");
